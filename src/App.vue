00cf8a1e875e52823a57c56cc593f0a9ed8497ce
Lina <akarabutova59@gmail.com>
2022-11-17 19:08:38 +0300

    initi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <ul>
+        <li v-for="item in items" :key="item.idx">
+          <CheckBox :item="item" msg="Welcome to Your Vue.js App" />
+        </li>
+      </ul>
+      <CheckBox msg="Welcome to Your Vue.js App" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CheckBox from "./components/CheckBox.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CheckBox,
+  },
+  data() {
+    return {
+      items: [
+        {
+          title: "title",
+          checked: false,
+          categories: [
+            { title: "title", checked: false },
+            { title: "title", checked: false },
+            { title: "title", checked: false },
+            {
+              title: "title",
+              checked: false,
+              categories: [
+                { title: "title", checked: true },
+                { title: "title", checked: false },
+                { title: "title", checked: false },
+                { title: "title", checked: false },
+                {
+                  title: "title",
+                  checked: false,
+                  categories: [
+                    { title: "title", checked: false },
+                    { title: "title", checked: false },
+                    { title: "title", checked: false },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "title",
+          checked: false,
+          categories: [
+            { title: "title", checked: false },
+            { title: "title", checked: false },
+            { title: "title", checked: false },
+            { title: "title", checked: false },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10rem;
 }
 </style>
